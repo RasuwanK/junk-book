4a69d926b5a1f5ae5664508c0229ddc0ea4c2aaa
Rasuwan Kalhara <49600817+RasuwanK@users.noreply.github.com>
2023-02-06 20:15:42 +0530

    More query functions

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -2,6 +2,9 @@ import { firestore } from "@/lib/firebase-services";
 import {
   getDocs,
   collection,
+  query,
+  where,
+  Timestamp,
 } from "firebase/firestore";
 import type { JunkType } from "@/firestore-orm/junks";
 import junkConverter from "@/firestore-orm/converters/junk-converter";
@@ -16,6 +19,20 @@ export async function getAllJunks() {
       result.push(doc.data());
     });
   } catch (e) {
+    throw new Error("Error while getting data from firestore " + e);
+  }
+
+  return result;
+}
+
+export async function getJunksByDate(date: Date) {
+  let result: JunkType[] = [];
+  try {
+    const junksCollection = collection(firestore, "junks").withConverter(junkConverter);
+    const junksQuery = query(junksCollection, where("dateAdded", ">=", Timestamp.fromDate(date)));
+    const querySnapshot = await getDocs(junksQuery);
+    querySnapshot.forEach(snap => result.push(snap.data()));
+  } catch(e) {
     throw new Error("Error while getting data from firestore " + e);
   }
 
