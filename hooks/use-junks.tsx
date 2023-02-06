93b916850570ee8db879fe91618d12253b85283a
Rasuwan Kalhara <49600817+RasuwanK@users.noreply.github.com>
2023-02-06 20:15:26 +0530

    Hook fetch data based on the date

--- hooks/use-junks.tsx
+++ hooks/use-junks.tsx
@@ -1,27 +1,31 @@
 import { useEffect, useState } from "react"
-import { getAllJunks } from "@/lib/firestore";
+import { getJunksByDate } from "@/lib/firestore";
 import type { JunkType } from "@/firestore-orm/junks";
 
-export default function useJunks() {
+export default function useJunks(date: Date | undefined) {
     // This hook is used to get and set junks (stories) to the database
     const [junks, setJunks] = useState<JunkType[]>([]);
     const [isLoading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<any>(null);
-    
+
     useEffect(() => {
         // Taking all the 'junk' from the database
-        getAllJunks()
-            .then((data) => {
-                // Data returned successfully
-                setJunks(data);
-                setLoading(false);
-            })
-            .catch((error) => {
-                // Error while returning data
-                setError(error);
-                setLoading(false);
-            });
-    }, []);
+        if (date) {
+            // If the hydration takes longer to get router query
+            getJunksByDate(date)
+                .then((data) => {
+                    // Data returned successfully
+                    setJunks(data);
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    // Error while returning data
+                    setError(error);
+                    setLoading(false);
+                });
+        }
+
+    }, [date]);
 
     return {
         junks,
